ede25fb40dad676b7b3ff33b39b182791ca279e4
Dharmin <dharmingeo@gmail.com>
2019-06-08 18:56:16 +0200

    Added catkin workspace switcher

---
+++ i3/catkin_workspace_switcher.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+current_dir="`dirname \"$0\"`"              # relative
+current_dir="`( cd \"$current_dir\" && pwd )`"  # absolutized and normalized
+catkin_workspaces_file="$current_dir""/catkin_workspaces"
+catkin_workspaces=`cat $catkin_workspaces_file`
+chosen_workspace=$(echo "$catkin_workspaces" | rofi -dmenu -i -fn "Monospace-13" -p "Which catkin_workspace?")
+if [ "$chosen_workspace" = "" ] 
+then
+    exit 1
+fi
+chosen_catkin_workspace_file="$current_dir""/chosen_catkin_workspace"
+echo "$chosen_workspace" > $chosen_catkin_workspace_file
+echo "Chosen catkin workspace: ""$chosen_workspace"
+echo "####################################"
+echo "# Please execute \`source ~/.zshrc\` #"
+echo "####################################"
--- i3/pre_lock.sh
+++ i3/pre_lock.sh
@@ -3,7 +3,7 @@
 scrot /tmp/screen.png
 
 [[ -f ~/Pictures/thinking.png ]] &&
-    convert /tmp/screen.png -scale 10% -scale 1000% -swirl -360 ~/Pictures/thinking.png -gravity center -composite -matte /tmp/screenlock.png
+    convert /tmp/screen.png -scale 5% -scale 2015% ~/Pictures/lock_batman.png -gravity center -composite -matte /tmp/screenlock.png
 
 # pause music
 playerctl pause
