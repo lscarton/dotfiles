62d7fd39b9501426fbd22441faee0f95e8632cf3
Dharmin <dharmingeo@gmail.com>
2020-01-14 20:19:29 +0100

    Updated beamer script to scale when clone is being used

--- i3/beamer.sh
+++ i3/beamer.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 
-# # check if there are multiple connected outputs or not
-# screen=$( xrandr | grep " connected" | wc -l )
-# # if 2 connected outputs, make HDMI same as normal display
-# if [[ $screen -eq 2 ]]
-# then
-#     choice=$(printf "Enable\nDisable\n" | dmenu -i -p "What should be done with second screen?")
-#     if [[ "$choice" = "Enable" ]]
-#     then
-#         xrandr --output HDMI-1 --mode 1360x768 --same-as eDP-1
-#     else
-#         xrandr --output HDMI-1 --off
-#     fi
-# # else turn HDMI off
-# else
-#     xrandr --output HDMI-1 --off
-# fi
-
-#!/bin/bash
-
 INTERNAL_OUTPUT="eDP1"
-
-choices="laptop\ndual\nexternal\nclone"
-
-chosen=$(echo -e $choices | rofi -dmenu -i -fn "Monospace-13" -p "which configuration?")
+EXTERNAL_OUTPUT="HDMI1"
 
 # Detect extenal monitor
 if [ `xrandr | grep HDMI1 | grep -c ' connected '` -eq 1 ]; then
-	EXTERNAL_OUTPUT="HDMI1"
+
+    choices="laptop\ndual\nexternal\nclone"
+
+    chosen=$(echo -e $choices | rofi -dmenu -i -fn "Monospace-13" -p "which configuration?")
+    case "$chosen" in
+        external)
+            xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto --primary
+            notify-send "Screen Configuration" "EXTERNAL"
+            ;;
+        laptop)
+            xrandr --output $INTERNAL_OUTPUT --auto --primary --output $EXTERNAL_OUTPUT --off
+            notify-send "Screen Configuration" "LAPTOP"
+            ;;
+        clone)
+            resolution=$(xrandr | grep -m 1 '*+' | awk '{print $1}')
+            echo $resolution
+            xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --scale-from $resolution
+            notify-send "Screen Configuration" "CLONE"
+            ;;
+        dual)
+            xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --right-of $INTERNAL_OUTPUT --primary
+            notify-send "Screen Configuration" "DUAL"
+            ;;
+    esac
+else
+    xrandr --output $INTERNAL_OUTPUT --auto --primary --output $EXTERNAL_OUTPUT --off
+    notify-send "Screen Configuration" "LAPTOP (default)"
 fi
 
-case "$chosen" in
-    external) xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto --primary ;;
-    laptop) xrandr --output $INTERNAL_OUTPUT --auto --primary --output $EXTERNAL_OUTPUT --off ;;
-    clone) xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --same-as $INTERNAL_OUTPUT ;;
-    dual) xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --right-of $INTERNAL_OUTPUT --primary ;;
-esac
