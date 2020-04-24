90bd5e52397cfe2c94bb8c44f34c44ea10352964
Dharmin <dharmingeo@gmail.com>
2020-04-24 19:35:23 +0200

    Improved the script for background gif

--- scripts/gif_selector.sh
+++ scripts/gif_selector.sh
@@ -1,3 +1,30 @@
-#! /bin/sh
+#! /bin/bash
 
-xwinwrap -g 1366x768 -ov -ni -s -nf -- gifview -w WID ~/Pictures/gif_wallpapers/$(ls -1 ~/Pictures/gif_wallpapers | rofi -dmenu -i -fn "Monospace-13" -p "Which gif?") -a
+screen_width=1366
+screen_height=768
+# get gif from a specific folder
+gif_name=$HOME/Pictures/gif_wallpapers/$(ls -1 ~/Pictures/gif_wallpapers | rofi -dmenu -i -fn "Monospace-13" -p "Which gif?")
+
+# get width and height of selected gif
+file_output="$(file $gif_name -b)"
+IFS=','; arrIN=($file_output); unset IFS;
+size="${arrIN[2]}"
+IFS='x'; arrIN=($size); unset IFS;
+width="${arrIN[0]}"
+height="${arrIN[1]}"
+
+# set a bg based on the first frame of the gif
+convert "$gif_name""[0]" /tmp/gif_bg.png
+convert /tmp/gif_bg.png -scale 10% -scale 1366x768 /tmp/gif_bg.png
+convert /tmp/gif_bg.png -blur 0x50 /tmp/gif_bg.png
+feh --bg-scale /tmp/gif_bg.png
+rm /tmp/gif_bg.png
+
+# calculate offset for the selected gif based on screen size
+width=$(($width))
+height=$(($height))
+width_offset=$(($screen_width/2 - $width/2))
+height_offset=$(($screen_height/2 - $height/2))
+
+# start xwinwrap to loop selected gif as background
+xwinwrap -g "$width""x""$height""+""$width_offset""+""$height_offset" -ov -ni -s -nf -- gifview -w WID $gif_name -a
--- scripts/gif_wallpaper.sh
+++ scripts/gif_wallpaper.sh
@@ -3,7 +3,7 @@
 case $BLOCK_BUTTON in 
     1) notify-send "Press mod + g to start GIF background";;
 
-    3) kill `pgrep xwinwrap` ;;
+    3) kill `pgrep xwinwrap`; feh --bg-scale ~/Pictures/Wallpapers/blue_material_design.jpg;;
 esac
 
 echo "GIF"
