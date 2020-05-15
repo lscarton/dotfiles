5fea30748042da5e9b97dc60f580c26d572d0e02
Dharmin <dharmingeo@gmail.com>
2020-05-15 13:56:20 +0200

    Fixed alarm script. It has 2 modes (time and duration)

--- alarm.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/sh
-sleep 5s && notify-send "wake up" && aplay ~/Music/Loud_Alarm_Clock_Buzzer-Muk1984-493547174.wav
---
+++ scripts/alarm.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+if [[ $1 == "-t" ]]; then
+    echo "time"
+    args=$@
+    time=("${args[@]:3}")
+    current_time=$(date +"%H:%M")
+    echo $time
+    while [[ $current_time != $time ]]; do
+        sleep 5s
+        current_time=$(date +"%H:%M")
+    done
+    i3-msg -t command "workspace 5"
+    notify-send "wake up" && aplay ~/Music/Loud_Alarm_Clock_Buzzer.wav
+elif [[ $1 == "-d" ]]; then
+    echo "duration"
+    args=$@
+    time=("${args[@]:3}")
+    sleep $time 
+    i3-msg -t command "workspace 5"
+    notify-send "wake up" && aplay ~/Music/Loud_Alarm_Clock_Buzzer.wav
+else
+    echo "use either -t (time in HH:MM) or -d (duration in sleep command format) option"
+    echo "Example:"
+    echo "    -t 5m 5s"
+    echo "    -d 15:55"
+fi
