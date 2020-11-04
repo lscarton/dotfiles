22e1fd9db4c7990f0b3d51463a45c4f6e2aa952e
Dharmin <dharmingeo@gmail.com>
2020-11-04 15:40:47 +0100

    [i3] Using light for changing brightness

--- scripts/change_brightness.sh
+++ scripts/change_brightness.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 
-if [[ -f ~/.config/display_name && -f ~/.config/brightness ]]; then
-    echo "present"
-else
-    echo "Display name not known or brightness file missing"
-    exit 1
-fi
-
-BRIGHTNESS="$(cat ~/.config/brightness)"
 if [ "$1" = "up" ]
 then
-    BRIGHTNESS=$(echo "$BRIGHTNESS+0.05" | bc)
-    if [ "$(echo "$BRIGHTNESS > 1.0" | bc)" -eq 1 ]; then
-        BRIGHTNESS='1.0'
+    light -A 2%
+    if [ "$2" = "big" ]
+    then
+        light -A 8%
     fi
 else
-    BRIGHTNESS=$(echo "$BRIGHTNESS-0.05" | bc)
-    if [ "$(echo "$BRIGHTNESS < 0.0" | bc)" -eq 1 ]; then
-        BRIGHTNESS='0.0'
+    light -U 2%
+    if [ "$2" = "big" ]
+    then
+        light -U 8%
     fi
 fi
-DISPLAY_NAME="$(cat ~/.config/display_name)"
-xrandr --output $DISPLAY_NAME --brightness $BRIGHTNESS
-echo $BRIGHTNESS > ~/.config/brightness
 
-BRIGHTNESS_PERC=$(echo "$BRIGHTNESS*100" | bc)
+BRIGHTNESS_PERC=$(light -G)
 s="Brightness level:" 
 notify-send -t 500 "$s" "$BRIGHTNESS_PERC"
