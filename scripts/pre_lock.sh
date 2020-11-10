9ef8476a02d7b7afca9bd342e4c56608d0215169
Dharmin <dharmingeo@gmail.com>
2020-11-10 13:51:42 +0100

    [i3] Added blur image in prelock

--- scripts/pre_lock.sh
+++ scripts/pre_lock.sh
@@ -3,7 +3,7 @@
 scrot /tmp/screen.png
 
 [[ -f ~/Pictures/lock_batman.png ]] &&
-    convert /tmp/screen.png -scale 5% -scale 2015% ~/Pictures/lock_batman.png -gravity center -composite -matte /tmp/screenlock.png
+    convert /tmp/screen.png -scale 5% -scale 2000% -blur 0x10 ~/Pictures/lock_batman.png -gravity center -composite -matte /tmp/screenlock.png
 
 # pause music
 playerctl pause
