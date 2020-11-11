c57e2ff76d2b8f17964efd35d1ba1f5fdcbbb8c6
Dharmin <dharmingeo@gmail.com>
2020-11-11 11:34:03 +0100

    [i3] Fixed lock script

--- scripts/pre_lock.sh
+++ scripts/pre_lock.sh
@@ -2,8 +2,8 @@
 
 scrot /tmp/screen.png
 
-[[ -f ~/Pictures/lock_batman.png ]] &&
-    convert /tmp/screen.png -scale 5% -scale 2000% -blur 0x10 ~/Pictures/lock_batman.png -gravity center -composite -matte /tmp/screenlock.png
+[[ -f ~/Pictures/lock.png ]] &&
+    convert /tmp/screen.png -scale 5% -scale 2000% ~/Pictures/lock.png -gravity center -composite -matte /tmp/screenlock.png
 
-# pause music
+## pause music
 playerctl pause
