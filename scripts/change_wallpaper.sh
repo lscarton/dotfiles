256f6a49d5732ec18ef736eb026a43ce59bd82ae
Dharmin <dharmingeo@gmail.com>
2022-11-10 15:14:39 +0100

    [scripts][change_wallpaper] Removed unnecessary prints

--- scripts/change_wallpaper.sh
+++ scripts/change_wallpaper.sh
@@ -3,8 +3,8 @@ export DISPLAY=:0.0
 wallpaperdir="$HOME/Pictures/Wallpapers"
 
 randompic=$(find $wallpaperdir -maxdepth 1 -type f | shuf -n1)
-echo $randompic
+# echo $randompic
 
 feh --bg-scale "$randompic"
 datetime=$(date -u)
-echo $datetime
+# echo $datetime
