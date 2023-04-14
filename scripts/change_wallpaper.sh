2200028a9b56c13405595889b99a59dba9a08c0a
Dharmin <dharmingeo@gmail.com>
2023-04-14 09:07:51 +0200

    [script][change_wallpaper] Fixed export display issue

--- scripts/change_wallpaper.sh
+++ scripts/change_wallpaper.sh
@@ -3,7 +3,7 @@
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
 
 if [[ $1 == "cronjob" ]]; then
-    export DISPLAY=:0.0
+    export DISPLAY=:1
 fi
 
 randompic=$(find $WALLPAPER_DIR -maxdepth 1 -type f | shuf -n1)
